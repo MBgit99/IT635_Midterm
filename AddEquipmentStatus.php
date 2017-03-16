811200b69a37baa50650ffe8ad48c055c92c56be
Mike Belgiovine <mb569@njit.edu>
2017-03-16 15:32:41 -0400

    Fixed Status Checking

--- AddEquipmentStatus.php
+++ AddEquipmentStatus.php
@@ -4,6 +4,7 @@ $jobID = $argv[1];
 $machineID = $argv[2];
 $startDate = $argv[3];
 $endDate = $argv[4];
+$status = $argv[5];
 
 $db = new mysqli('localhost','root','SUPERrootTREE!','IronWorks');
 if ($db->connect_errno > 0)
@@ -11,18 +12,51 @@ if ($db->connect_errno > 0)
    echo __FILE__.":".__LINE__.": failed to connect to db, re: $db->connect_error".PHP_EOL;
    exit(0);
 }
+
+
+echo "\r\n";
 echo "checking if machine in use: $machineID".PHP_EOL;
-$checkMachine = "select * from equipmentStatus WHERE (endDate = '0000-00-00') AND machineID = $machineID;";
-$checkResult = $db->query($checkMachine);
-if (!empty($checkResult)) {
-	exit("Machine in use!\r\n");
-}
-echo "attempting to insert record: $jobID, $machineID, $startDate, $endDate".PHP_EOL;
-if($endDate == 'none' || 'NONE') {
-	$insertString = "insert into equipmentStatus(jobID, machineID, startDate) values ('$jobID', '$machineID', '$startDate');";
+$checkMachine = $db->query("select * from equipmentStatus WHERE status IN ('in use', 'IN USE', 'In Use') AND machineID = $machineID;");
+//$result1 = mysqli_fetch_array($checkMachine);
+
+if (mysqli_num_rows($checkMachine) > 0) {
+		exit("Machine in use!\r\n");
 }
 else {
-$insertString = "insert into equipmentStatus(jobID, machineID, startDate, endDate) values ('$jobID', '$machineID', '$startDate', 'endDate');";
+	echo "Machine is Free!\r\n";
+	echo "\r\n";
+}
+
+
+
+
+echo "checking if machine is functional: $machineID".PHP_EOL;
+
+$machineStatus = $db->query("select * from equipmentStatus WHERE status IN ('broken','BROKEN','Broken') AND machineID = $machineID;");
+
+if (mysqli_num_rows($machineStatus) > 0) {
+		exit("Machine is Broken!!!\r\n");
+}
+else {
+	echo "Machine is Operational!!\r\n";
+}
+
+
+
+
+echo "\r\n";
+echo "attempting to insert record: $jobID, $machineID, $startDate, $endDate".PHP_EOL;
+if($endDate == 'none' || 'NONE') {
+	$insertString = "insert into equipmentStatus(jobID, machineID, startDate, status) values ('$jobID', '$machineID', '$startDate', '$status');";
+}
+elseif($jobID == 'none' || 'NONE'){
+$insertString = "insert into equipmentStatus(machineID, startDate, endDate, status) values ('$machineID', '$startDate', 'endDate', '$status');";
+}
+elseif(($jobID == 'none' || 'NONE') && ($endDate == 'none' || 'NONE')){
+$insertString = "insert into equipmentStatus(machineID, startDate, status) values ('$machineID', '$startDate', '$status');";
+}
+else {
+	$insertString = "insert into equipmentStatus(jobID, machineID, startDate, endDate, status) values ('$jobID','$machineID', '$startDate', 'endDate', '$status');";
 }
 echo "attempting to execute this SQL:".PHP_EOL;
 echo $insertString.PHP_EOL;
