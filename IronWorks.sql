849da9863c1372f14b51a09e804e69c65e4cd70b
Mike Belgiovine <mb569@njit.edu>
2017-05-10 17:56:22 -0400

    Final Commit

--- IronWorks.sql
+++ IronWorks.sql
@@ -1,8 +1,8 @@
--- MySQL dump 10.13  Distrib 5.7.17, for Linux (x86_64)
+-- MySQL dump 10.13  Distrib 5.7.18, for Linux (x86_64)
 --
 -- Host: localhost    Database: IronWorks
 -- ------------------------------------------------------
--- Server version	5.7.17-0ubuntu0.16.04.1-log
+-- Server version	5.7.18-0ubuntu0.16.04.1-log
 
 /*!40101 SET @OLD_CHARACTER_SET_CLIENT=@@CHARACTER_SET_CLIENT */;
 /*!40101 SET @OLD_CHARACTER_SET_RESULTS=@@CHARACTER_SET_RESULTS */;
@@ -51,8 +51,11 @@ CREATE TABLE `equipment` (
   `location` varchar(255) DEFAULT NULL,
   `lockable` varchar(3) DEFAULT NULL,
   `typeOfLock` varchar(255) DEFAULT NULL,
+  `nextMaintenance` date DEFAULT NULL,
+  `lastMaintenance` date DEFAULT NULL,
+  `schedule` varchar(255) DEFAULT NULL,
   PRIMARY KEY (`machineID`)
-) ENGINE=InnoDB AUTO_INCREMENT=30 DEFAULT CHARSET=latin1;
+) ENGINE=InnoDB AUTO_INCREMENT=32 DEFAULT CHARSET=latin1;
 /*!40101 SET character_set_client = @saved_cs_client */;
 
 --
@@ -61,7 +64,7 @@ CREATE TABLE `equipment` (
 
 LOCK TABLES `equipment` WRITE;
 /*!40000 ALTER TABLE `equipment` DISABLE KEYS */;
-INSERT INTO `equipment` VALUES ('Band Saw',' Hyd-Mech M-20A',1,' Electrical / Hydraulic',' Bay B: South Wall',' Y',' Padlock/Tag'),('Band Saw',' Peddinghaus Model 30/16',2,' Electrical / Hydraulic',' Bay D: South Wall',' Y',' Padlock/Tag'),('Band Saw',' Peddinghaus DG 1100',3,' Electrical / Hydraulic',' Bay E: South Wall',' Y',' Padlock/Tag'),('Drill Line',' Peddinghaus Model PCD 1100/C',4,' Electrical / Hydraulic',' Bay E: South Wall',' Y',' Padlock/Tag'),('Drill Line',' Peddinghaus BDL-760',5,' Electrical / Hydraulic',' Bay D: South Wall',' Y',' Padlock/Tag'),('Plate Processor',' Peddinghaus FPB 1800',6,' Electrical / Hydraulic',' Bay C: North Wall',' Y',' Padlock/Tag'),('Angle Master',' Peddinghaus Model 623M',7,' Electrical / Hydraulic',' Bay B: South Wall',' Y',' Padlock/Tag'),('220 Ton Ironworker Machine',' Geka Hydracrop 220/SD',8,' Electrical / Hydraulic',' Bay B: North Wall',' Y',' Padlock/Tag'),('Plate Shear',' Baykal Fab-Line MGH 3100 x 20',9,' Electrical / Hydraulic',' Bay B: Northwest Wall',' Y',' Padlock/Tag'),('Press Brake',' Baykal Primeline',10,' Electrical / Hydraulic',' Bay B: Northwest Wall',' Y',' Padlock/Tag'),('Cambering Machine',' CambCo Model 1700',11,' Electrical / Hydraulic',' Bay B: North Wall',' Y',' Padlock/Tag'),('Overhead Crane',' 15 Ton Crane: P&H Trav-Lift Serial #: 27112',13,' Electrical',' Bay E',' Y',' Padlock/Tag'),('Overhead Crane',' 7.5 Ton Crane: R&M Serial #: A9901178',14,' Electrical',' Bay D',' Y',' Padlock/Tag'),('Overhead Crane',' 10 Ton Crane: Yale Serial #: G35772WE',15,' Electrical',' Bay D',' Y',' Padlock/Tag'),('Overhead Crane',' 5 Ton Crane: Electro-Lift Serial #: 64127R',16,' Electrical',' Bay C',' Y',' Padlock/Tag'),('Overhead Crane',' 10 Ton Crane: P&H Serial #: 1-60441',17,' Electrical',' Bay C',' Y',' Padlock/Tag'),('Overhead Crane',' 10 Ton Crane: P&H Serial #: 1-61363',18,' Electrical',' Bay B',' Y',' Padlock/Tag'),('Overhead Crane',' 5 Ton Crane: Shaw Box Serial #: K1-73648',19,' Electrical',' Bay B',' Y',' Padlock/Tag'),('Overhead Crane',' 5 Ton Crane: Yale Serial #: 12052170',20,' Electrical',' Bay A',' Y',' Padlock/Tag'),('Overhead Crane',' 5 Ton Crane: Shaw Box Serial #: K2-59300',21,' Electrical',' Bay A',' Y',' Padlock/Tag'),('BigMachine','999',23,'Electric','Bay Z','Y','PadLock'),('TestMachine2','Big Damn Model',24,'electric','Bay D','Y','Padlock'),('Testing-Machine','Testing-Model',25,'Fires of hell','Bay X','Y','Padlock'),('Last Test','Testing Model',26,'Students Hopes and Dreams','IT635','Y','Souls'),('Big Fn Machine','Enormous',27,'Celestial Wind','Bay Z','N','NULL'),('KehoeMachine','Sarcastic',28,'Childrens Souls','Kehoes Pub','N','NULL'),('ANGRY MACHINE','9989',29,'POWER','SOMEWHERE','N','NULL');
+INSERT INTO `equipment` VALUES ('Band Saw',' Hyd-Mech M-20A',1,' Electrical / Hydraulic',' Bay B: South Wall',' Y',' Padlock/Tag','2017-05-19','2017-05-10','3 Months'),('Band Saw',' Peddinghaus Model 30/16',2,' Electrical / Hydraulic',' Bay D: South Wall',' Y',' Padlock/Tag','2017-06-11','2017-02-11','2 Months'),('Band Saw',' Peddinghaus DG 1100',3,' Electrical / Hydraulic',' Bay E: South Wall',' Y',' Padlock/Tag','2017-07-04','2017-03-07','3 Months'),('Drill Line',' Peddinghaus Model PCD 1100/C',4,' Electrical / Hydraulic',' Bay E: South Wall',' Y',' Padlock/Tag','2017-08-01','2017-02-14','6 Months'),('Drill Line',' Peddinghaus BDL-760',5,' Electrical / Hydraulic',' Bay D: South Wall',' Y',' Padlock/Tag','2017-05-19','2017-01-09','2 Months'),('Plate Processor',' Peddinghaus FPB 1800',6,' Electrical / Hydraulic',' Bay C: North Wall',' Y',' Padlock/Tag','2017-05-15','2017-01-09','3 Months'),('Angle Master',' Peddinghaus Model 623M',7,' Electrical / Hydraulic',' Bay B: South Wall',' Y',' Padlock/Tag','2017-05-10','2017-03-09','6 Months'),('220 Ton Ironworker Machine',' Geka Hydracrop 220/SD',8,' Electrical / Hydraulic',' Bay B: North Wall',' Y',' Padlock/Tag',NULL,NULL,'4 Months'),('Plate Shear',' Baykal Fab-Line MGH 3100 x 20',9,' Electrical / Hydraulic',' Bay B: Northwest Wall',' Y',' Padlock/Tag',NULL,NULL,'4 Months'),('Press Brake',' Baykal Primeline',10,' Electrical / Hydraulic',' Bay B: Northwest Wall',' Y',' Padlock/Tag','2017-09-01','2017-02-02','4 Months'),('Cambering Machine',' CambCo Model 1700',11,' Electrical / Hydraulic',' Bay B: North Wall',' Y',' Padlock/Tag','2017-05-11',NULL,'4 Months'),('Overhead Crane',' 15 Ton Crane: P&H Trav-Lift Serial #: 27112',13,' Electrical',' Bay E',' Y',' Padlock/Tag',NULL,NULL,'4 Months'),('Overhead Crane',' 7.5 Ton Crane: R&M Serial #: A9901178',14,' Electrical',' Bay D',' Y',' Padlock/Tag',NULL,NULL,'4 Months'),('Overhead Crane',' 10 Ton Crane: Yale Serial #: G35772WE',15,' Electrical',' Bay D',' Y',' Padlock/Tag',NULL,NULL,'4 Months'),('Overhead Crane',' 5 Ton Crane: Electro-Lift Serial #: 64127R',16,' Electrical',' Bay C',' Y',' Padlock/Tag',NULL,NULL,'4 Months'),('Overhead Crane',' 10 Ton Crane: P&H Serial #: 1-60441',17,' Electrical',' Bay C',' Y',' Padlock/Tag',NULL,NULL,'4 Months'),('Overhead Crane',' 10 Ton Crane: P&H Serial #: 1-61363',18,' Electrical',' Bay B',' Y',' Padlock/Tag',NULL,NULL,'4 Months'),('Overhead Crane',' 5 Ton Crane: Shaw Box Serial #: K1-73648',19,' Electrical',' Bay B',' Y',' Padlock/Tag',NULL,NULL,'4 Months'),('Overhead Crane',' 5 Ton Crane: Yale Serial #: 12052170',20,' Electrical',' Bay A',' Y',' Padlock/Tag',NULL,NULL,'4 Months'),('Overhead Crane',' 5 Ton Crane: Shaw Box Serial #: K2-59300',21,' Electrical',' Bay A',' Y',' Padlock/Tag',NULL,NULL,'4 Months'),('BigMachine','999',23,'Electric','Bay Z','Y','PadLock',NULL,NULL,'4 Months'),('TestMachine2','Big Damn Model',24,'electric','Bay D','Y','Padlock',NULL,NULL,'4 Months'),('Testing-Machine','Testing-Model',25,'Fires of hell','Bay X','Y','Padlock',NULL,NULL,'4 Months'),('Last Test','Testing Model',26,'Students Hopes and Dreams','IT635','Y','Souls',NULL,NULL,'4 Months'),('Big Fn Machine','Enormous',27,'Celestial Wind','Bay Z','N','NULL',NULL,NULL,'4 Months'),('KehoeMachine','Sarcastic',28,'Childrens Souls','Kehoes Pub','N','NULL',NULL,NULL,'4 Months'),('ANGRY MACHINE','9989',29,'POWER','SOMEWHERE','N','NULL',NULL,NULL,'4 Months'),('PLEASEWORK','workingmachine',30,'something that works','Bay C','N','NULL',NULL,NULL,'4 Months'),('TestingAgain','10099',31,'Brotein','Bay G','Y','Padlock',NULL,NULL,'4 Months');
 /*!40000 ALTER TABLE `equipment` ENABLE KEYS */;
 UNLOCK TABLES;
 
@@ -202,6 +205,36 @@ INSERT INTO `materials` VALUES ('I Beam',101,10,' Fireproof',2),('W Beam',102,20
 UNLOCK TABLES;
 
 --
+-- Table structure for table `parts`
+--
+
+DROP TABLE IF EXISTS `parts`;
+/*!40101 SET @saved_cs_client     = @@character_set_client */;
+/*!40101 SET character_set_client = utf8 */;
+CREATE TABLE `parts` (
+  `partID` int(11) NOT NULL AUTO_INCREMENT,
+  `partName` varchar(255) NOT NULL,
+  `brand` varchar(255) DEFAULT NULL,
+  `vendor` varchar(255) DEFAULT NULL,
+  `price` decimal(19,2) DEFAULT NULL,
+  `machineID` int(11) NOT NULL,
+  PRIMARY KEY (`partID`),
+  KEY `machineID` (`machineID`),
+  CONSTRAINT `parts_ibfk_1` FOREIGN KEY (`machineID`) REFERENCES `equipment` (`machineID`)
+) ENGINE=InnoDB AUTO_INCREMENT=13 DEFAULT CHARSET=latin1;
+/*!40101 SET character_set_client = @saved_cs_client */;
+
+--
+-- Dumping data for table `parts`
+--
+
+LOCK TABLES `parts` WRITE;
+/*!40000 ALTER TABLE `parts` DISABLE KEYS */;
+INSERT INTO `parts` VALUES (1,'3/8 Saw Blade','Hyd-Mech','Borussia Dortmund',178.99,1),(2,'30/16 Saw Blade','Peddinghaus','Borussia Dortmund',192.99,2),(3,'11/3 Saw Blade','Peddinghaus','Borussia Dortmund',211.99,3),(4,'1 Ton Shear','Baykal','Inter Milan',445.99,9),(5,'Hydraulic Chamber','Baykal','Inter Milan',1099.99,9),(6,'3/8 Drill Head','Peddinghaus','Borussia  Dortmund',56.99,4),(7,'5/8Drill Head','Peddinghaus','Borussia Dortmund',59.99,5),(8,'3/8 Carbide Drill Head','Peddinghaus','Umbrella Corp.',49.99,6),(9,'MQL Lubrication','Peddinghaus','Umbrella Corp.',19.99,6),(10,'9/8 Carbide Drill Head','Peddinghaus','Umbrella Corp.',49.99,6),(11,'Hypertherm Plasma Cutting 260','Peddinghaus','Umbrella Corp.',299.99,6),(12,'Hypertherm Plasma Cutting 400','Peddinghaus','Umbrella Corp.',399.99,6);
+/*!40000 ALTER TABLE `parts` ENABLE KEYS */;
+UNLOCK TABLES;
+
+--
 -- Table structure for table `user`
 --
 
@@ -222,7 +255,7 @@ CREATE TABLE `user` (
 
 LOCK TABLES `user` WRITE;
 /*!40000 ALTER TABLE `user` DISABLE KEYS */;
-INSERT INTO `user` VALUES ('admin','admin',1,'Overlord'),('guest','guest',0,'Big Noob'),('employee','*2470C0C06DEE42FD1618BB99005ADCA2EC9D1E19',0,'workslave'),('workerman','*27D57276AC96BEA22D05B92B91E2CDB3FD0A2226',0,'morework'),('workerman','*27D57276AC96BEA22D05B92B91E2CDB3FD0A2226',0,'morework');
+INSERT INTO `user` VALUES ('admin','8c6976e5b5410415bde908bd4dee15dfb167a9c873fc4bb8a81f6f2ab448a918',1,'Overlord'),('guest','84983c60f7daadc1cb8698621f802c0d9f9a3c3c295c810748fb048115c186ec',0,'Big Noob'),('worker','87eba76e7f3164534045ba922e7770fb58bbd14ad732bbf5ba6f11cc56989e6e',0,'Workman'),('Testing Replication','9f86d081884c7d659a2feaa0c55ad015a3bf4f1b2b0b822cd15d6c15b0f00a08',0,'tester');
 /*!40000 ALTER TABLE `user` ENABLE KEYS */;
 UNLOCK TABLES;
 /*!40103 SET TIME_ZONE=@OLD_TIME_ZONE */;
@@ -235,4 +268,4 @@ UNLOCK TABLES;
 /*!40101 SET COLLATION_CONNECTION=@OLD_COLLATION_CONNECTION */;
 /*!40111 SET SQL_NOTES=@OLD_SQL_NOTES */;
 
--- Dump completed on 2017-04-28  9:27:57
+-- Dump completed on 2017-05-10 17:17:14
